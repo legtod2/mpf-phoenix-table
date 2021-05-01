8315bedf0e8cc6a48246cc778120e96237718eb1
legtod2 <83455042+legtod2@users.noreply.github.com>
2021-05-01 06:54:45 -0300

    Add files via upload

--- test_simple_game.py
+++ test_simple_game.py
@@ -8,6 +8,8 @@ class TestSimpleGame(FullMachineTestCase):
         return os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
 
     def test_single_player_game(self):
+        # Let attract mode cycle once
+        self.advance_time_and_run(30)
         # Prove coins work to get credit (takes 2 coins for 1 credit and game not on free play)
         self.hit_and_release_switch("s_left_coin")
         self.advance_time_and_run(.5)
@@ -60,8 +62,8 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(.5)
         self.hit_and_release_switch("s_lane5")
         self.advance_time_and_run()
-        # Score should add up to 29,300
-        self.assertEqual(29300, self.machine.game.player.score)
+        # Score should add up to 29,030
+        self.assertEqual(29030, self.machine.game.player.score)
         self.assertEqual(2, self.machine.game.player.multiplier)
         self.release_switch_and_run("s_flipper_left", 1)
 
@@ -91,8 +93,8 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(1)
         self.hit_and_release_switch("s_bullseye")
         self.advance_time_and_run(1)
-        # Score should add up to 64,300
-        self.assertEqual(64300, self.machine.game.player.score)
+        # Score should add up to 64,030
+        self.assertEqual(64030, self.machine.game.player.score)
         # Letters P & H Lit
         self.hit_and_release_switch("s_lane1")
         self.advance_time_and_run(.5)
@@ -106,8 +108,8 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(1)
         self.hit_and_release_switch("s_bullseye")
         self.advance_time_and_run(1)
-        # Score should add up to 119,300
-        self.assertEqual(119300, self.machine.game.player.score)
+        # Score should add up to 119,030
+        self.assertEqual(119030, self.machine.game.player.score)
         self.hit_and_release_switch("s_lane1")
         self.advance_time_and_run(.5)
         self.hit_and_release_switch("s_lane2")
@@ -118,13 +120,13 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(.5)
         self.hit_and_release_switch("s_lane5")
         self.advance_time_and_run(1)
-        # Score should add up to 154,300
-        self.assertEqual(154300, self.machine.game.player.score)
+        # Score should add up to 154,030
+        self.assertEqual(154030, self.machine.game.player.score)
 # Letters O & E Lit and so is extra ball lamp lit
         self.hit_and_release_switch("s_bullseye")
         self.advance_time_and_run(1)
-        # Score should add up to 174,300
-        self.assertEqual(174300, self.machine.game.player.score)
+        # Score should add up to 174,030
+        self.assertEqual(174030, self.machine.game.player.score)
 # Check to see if extraball awarded
         self.hit_and_release_switch("s_lane1")
         self.advance_time_and_run(.5)
@@ -156,8 +158,8 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(.5)
         self.hit_and_release_switch("s_lane5")
         self.advance_time_and_run(1)
-        # Score should add up to 319,300
-        self.assertEqual(319300, self.machine.game.player.score)
+        # Score should add up to 319,030
+        self.assertEqual(319030, self.machine.game.player.score)
 
         # ball 2 drains again but shoot again lit
         self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
@@ -170,13 +172,13 @@ class TestSimpleGame(FullMachineTestCase):
         self.advance_time_and_run(5) 
         self.hit_and_release_switch("s_lane1")
         self.advance_time_and_run(.5)
-        # Score should add up to 353,300
-        self.assertEqual(353300, self.machine.game.player.score)
+        # Score should add up to 353,030
+        self.assertEqual(353030, self.machine.game.player.score)
         # Prove score of spinner starts 100 and grow to 1000 when 10 bonus
         self.hit_and_release_switch("s_spinner")
         self.advance_time_and_run(.5)
         # Score should be increased by 100 pts
-        self.assertEqual(353400, self.machine.game.player.score)
+        self.assertEqual(353130, self.machine.game.player.score)
         self.hit_and_release_switch("s_left_outlane")
         self.advance_time_and_run(.5)
         self.hit_and_release_switch("s_left_outlane")
@@ -200,11 +202,11 @@ class TestSimpleGame(FullMachineTestCase):
         # Should have 11 bonus pts
         self.assertEqual(11, self.machine.game.player.bonus)
         # Score should be increased by 10,000 pts
-        self.assertEqual(363400, self.machine.game.player.score)
+        self.assertEqual(363130, self.machine.game.player.score)
         self.hit_and_release_switch("s_spinner")
         self.advance_time_and_run(.5)
         # Score should be increased by 1000 pts not 100 pts
-        self.assertEqual(364400, self.machine.game.player.score)
+        self.assertEqual(364130, self.machine.game.player.score)
  
         # ball 2 drains Shoot again over
         self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
@@ -226,20 +228,26 @@ class TestSimpleGame(FullMachineTestCase):
         self.assertSlideOnTop("high_score_enter_initials")
         self.hit_and_release_switch("s_flipper_right")
         self.hit_and_release_switch("s_flipper_right")
-        self.hit_and_release_switch("s_start")  # C
-        self.advance_time_and_run()
-        self.assertTextOnTopSlide("C")
         self.hit_and_release_switch("s_flipper_right")
-        self.hit_and_release_switch("s_start")  # CD
+        self.hit_and_release_switch("s_start")  # D
         self.advance_time_and_run()
-        self.assertTextOnTopSlide("CD")
+        self.assertTextOnTopSlide("D")
+        self.hit_and_release_switch("s_flipper_left")
+        self.hit_and_release_switch("s_flipper_left")
+        self.hit_and_release_switch("s_flipper_left")
+        self.hit_and_release_switch("s_start")  # DA
+        self.advance_time_and_run()
+        self.assertTextOnTopSlide("DA")
         self.hit_and_release_switch("s_flipper_right")
-        self.hit_and_release_switch("s_start")  # CDE
+        self.hit_and_release_switch("s_flipper_right")
+        self.hit_and_release_switch("s_flipper_right")
+        self.hit_and_release_switch("s_start")  # DAD
         self.advance_time_and_run()
-        self.assertTextOnTopSlide("CDE")
+        self.assertTextOnTopSlide("DAD")
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run()
 
         self.assertEventCalled('text_input_high_score_complete')
-        self.advance_time_and_run(10)
+        # Allow attract mode to cycle 1 loop
+        self.advance_time_and_run(30)
         self.assertIsNone(self.machine.game)
